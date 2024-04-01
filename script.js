262a95a4cb9077222aad4f94ebf854a0228ce257
“Manish” <manishpaulsimon@gmail.comgit config --global user.name Your>
2024-04-01 18:44:15 +0200

    Added the buttons and basic UI

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(a,b) => a + b;
+
+function subtract(a,b) => a - b;
+
+function multiply(a,b) => a * b;
+
+function divide (a,b) => a / b;
+
+
+let var1 = 0;
+let var2 = 0;
+let operator;
+
+function operate(op,var1,var2) {
+    if (op == '+') a+b;
+    if (op == '-') a-b;
+    if (op == '*') a*b;
+    if (op == '/') a/b;
+
+}
+
