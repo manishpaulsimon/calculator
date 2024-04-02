69f5bc44d73cd44c830e8e25411ee174609aee3c
“Manish” <manishpaulsimon@gmail.comgit config --global user.name Your>
2024-04-02 12:32:29 +0200

    Added display functionality

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-function add(a,b) => a + b;
-
-function subtract(a,b) => a - b;
-
-function multiply(a,b) => a * b;
-
-function divide (a,b) => a / b;
-
+function add(a,b) {a + b};
+function subtract(a,b) {a - b};
+function multiply(a,b) {a * b};
+function divide (a,b) {a / b};
 
 let var1 = 0;
 let var2 = 0;
 let operator;
 
 function operate(op,var1,var2) {
-    if (op == '+') a+b;
-    if (op == '-') a-b;
-    if (op == '*') a*b;
-    if (op == '/') a/b;
-
+    if (op == '+') add(var1,var2);
+    if (op == '-') subtract(var1,var2);
+    if (op == '*') multiply(var1,var2);
+    if (op == '/') divide(var1,var2);
 }
 
+function display() {
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => { 
+        button.addEventListener('click',(e) => {
+            const newDisplay = document.querySelector(".display");
+            if (newDisplay) {
+                newDisplay.textContent = e.target.textContent;
+            }
+        });
+    });
+}
+display();
